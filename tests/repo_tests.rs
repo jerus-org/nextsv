1b2c47f6ecb145284d4b21ff60a8d6e8341cfa70
Jeremiah Russell <jerry@jrussell.ie>
2024-04-22 08:31:30 +0100

    test: Add support for pre-release versions in test_repo_with_commit function

--- tests/repo_tests.rs
+++ tests/repo_tests.rs
@@ -32,6 +32,14 @@ fn test_repo_no_changes() {
 #[rstest]
 #[case::non_production_commit("v0.1.0", "-n")]
 #[case::production_commit("v1.1.0", "-n")]
+#[case::non_production_pre_release_alpha_commit("v0.1.0-alpha.2", "-n")]
+#[case::production_pre_release_alpha_commit("v1.1.0-alpha.3", "-n")]
+#[case::non_production_pre_release_beta_commit("v0.1.0-beta.4", "-n")]
+#[case::production_pre_release_beta_commit("v1.1.0-beta.5", "-n")]
+#[case::non_production_pre_release_rc_commit("v0.1.0-rc.6", "-n")]
+#[case::production_pre_release_rc_commit("v1.1.0-rc.7", "-n")]
+#[case::non_production_pre_release_pre_commit("v0.1.0-pre.8", "-n -vvvv")]
+#[case::production_pre_release_pre_commit("v1.1.0-pre.9", "-n -vvvv")]
 #[trace]
 fn test_repo_with_commit(
     #[case] current_version: &str,
@@ -55,6 +63,46 @@ fn test_repo_with_commit(
             | "custom" | "build" => "patch\n1.1.1\n",
             "feat" => "minor\n1.2.0\n",
             "breaking" => "major\n2.0.0\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v0.1.0-alpha.2" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "alpha\n0.1.0-alpha.3\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v1.1.0-alpha.3" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "alpha\n1.1.0-alpha.4\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v0.1.0-beta.4" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "beta\n0.1.0-beta.5\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v1.1.0-beta.5" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "beta\n1.1.0-beta.6\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v0.1.0-rc.6" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "rc\n0.1.0-rc.7\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v1.1.0-rc.7" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "rc\n1.1.0-rc.8\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v0.1.0-pre.8" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "0.1.0-pre.9\n0.1.0-pre.9\n",
+            _ => panic!("unexpected commit type"),
+        },
+        "v1.1.0-pre.9" => match commit_type {
+            "fix" | "chore" | "ci" | "revert" | "docs" | "style" | "refactor" | "perf" | "test"
+            | "custom" | "build" | "breaking" | "feat" => "1.1.0-pre.10\n1.1.0-pre.10\n",
             _ => panic!("unexpected commit type"),
         },
         _ => panic!("unexpected current version"),
