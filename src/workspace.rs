d8a36c5e8bb5cdef31b46eee0fc017082f7d16da
Jeremiah Russell <jerry@jrussell.ie>
2024-10-26 07:46:37 +0100

    feat: add workspace module to handle Cargo.toml in workspace

---
+++ src/workspace.rs
@@ -0,0 +1,83 @@
+use std::path::Path;
+
+use crate::Error;
+use cargo_toml::Manifest;
+
+/// Represents the Cargo.toml of the workspace
+///
+/// See https://doc.rust-lang.org/cargo/reference/manifest.html#the-workspace
+///  
+#[derive(Debug, Clone)]
+pub struct Workspace {
+    /// The Cargo.toml of the workspace
+    pub manifest: Manifest,
+}
+
+impl Workspace {
+    /// Creates a new Workspace
+    ///
+    /// # Example
+    ///     
+    /// ```no_run
+    /// # fn main() -> Result<(),nextsv::Error> {
+    /// # use nextsv::Workspace;
+    ///     let workspace = Workspace::new("./Cargo.toml")?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new(ws_cargo_toml: &Path) -> Result<Self, Error> {
+        let manifest = Manifest::from_path(ws_cargo_toml)?;
+
+        Ok(Self { manifest })
+    }
+
+    /// Returns the list of packages in the workspace
+    ///     
+    /// # Example
+    ///     
+    /// ```no_run
+    /// # fn main() -> Result<(),nextsv::Error> {
+    /// # use nextsv::Workspace;
+    ///     let workspace = Workspace::new("./Cargo.toml")?;
+    ///     let packages = workspace.packages();
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    pub fn packages(&self) -> Option<Vec<Package>> {
+        if let Some(workspace) = &self.manifest.workspace {
+            let members = &workspace.members;
+
+            let mut packages = Vec::new();
+
+            for member in members {
+                let member_file = format!("./{member}/Cargo.toml");
+                let path = Path::new(&member_file);
+                let manifest = Manifest::from_path(path).unwrap();
+                if let Some(package) = manifest.package {
+                    let name = package.name;
+                    let version = package.version.get().unwrap().to_string();
+
+                    let package = Package {
+                        name,
+                        version,
+                        member: member.to_string(),
+                    };
+
+                    packages.push(package);
+                }
+            }
+
+            return Some(packages);
+        }
+
+        None
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub member: String,
+}
